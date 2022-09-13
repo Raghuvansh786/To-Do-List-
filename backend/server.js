3d9710d40e466da0bc79cf6562964c0cd79e3ecc
Raghuvansh786 <girir1@student.douglascollege.ca>
2022-09-13 16:30:36 -0700

    removed yarn.lock file

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
