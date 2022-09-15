e6b44351a2a67120cddbc4163228dedd0b36340e
Raghuvansh786 <girir1@student.douglascollege.ca>
2022-09-15 11:31:35 -0700

    Added  files to gitignore

---
+++ frontend/build/precache-manifest.7f4e7ed33a96fdd853aff15466644946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b53d49ac8fbec3f04b8",
+    "url": "/static/js/main.6b53d49a.chunk.js"
+  },
+  {
+    "revision": "ea4d7151a2d075154ba0",
+    "url": "/static/css/1.4ffe7946.chunk.css"
+  },
+  {
+    "revision": "ea4d7151a2d075154ba0",
+    "url": "/static/js/1.ea4d7151.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ef499f1ac4a4deeee42c3608716630c",
+    "url": "/index.html"
+  }
+];
